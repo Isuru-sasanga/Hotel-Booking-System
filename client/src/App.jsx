f4d4cb51c366365f2f3a67741985b28cdeca88b6
Isuru-sasanga <kolamba-se19038@stu.kln.ac.lk>
2023-07-05 00:26:19 +0530

    create: header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,29 @@ import './App.css'
 
 function App() {
   return (
-    <div className="bg-red-500">test</div>
+    <div>
+      <header className="p-4 flex justify-between">
+        <a href="" className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+          </svg>
+          <span className="font-bold text-xl">BoOkiNg</span>
+        </a>
+
+        <div className="flex gap-2 border rounded-full border-gray-300 py-2 px-4 shadow-md shadow-gray-300">
+          <div>Anywhere</div>
+          <div className="border-l border-gray-300"></div>
+          <div>Any week</div>
+          <div className="border-l border-gray-300"></div>
+          <div>Add guests</div>
+          <button className="bg-primary rounded-full text-white p-1.5">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+          </button>
+        </div>
+      </header>
+    </div>
   )
 }
 
