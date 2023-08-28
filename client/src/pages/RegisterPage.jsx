d1f90edf7d667be3809072f6b6efb3b17268fff1
Isuru-sasanga <kolamba-se19038@stu.kln.ac.lk>
2023-08-28 21:00:15 +0530

    add register states

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function RegisterPage(){
-    return (
-        <div className="mt-4 grow flex items-center justify-around">
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
 
         <form className="max-w-md mx-auto">
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+          <input type="text" placeholder="name" value={name} onChange={ev => setName(ev.target.value)}/>
+          <input type="email" placeholder="your@email.com" value={email} onChange={ev => setEmail(ev.target.value)}/> 
+          <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}/>
           <button className="login">Register</button>
           <div className="text-center py-2 text-gray-500">
             Already have an account? <Link className="underline text-black" to={'/login'}>Login Now</Link></div>
         </form>
+
       </div>
     </div>
-    );
+  );
 }
