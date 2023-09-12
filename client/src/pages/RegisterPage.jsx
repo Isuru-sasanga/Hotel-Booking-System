48343f66f1d485e145eae8fa46718e17c1229752
Isuru-sasanga <kolamba-se19038@stu.kln.ac.lk>
2023-09-12 14:48:15 +0530

    create  api

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function RegisterPage(){
   const [name,setName] = useState('');
   const [email,setEmail] = useState('');
   const [password,setPassword] = useState('');
 
+  function registerUser(ev){
+    ev.preventDefault();
+    axios.get('http://localhost:5000/test');
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
 
-        <form className="max-w-md mx-auto">
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
           <input type="text" placeholder="name" value={name} onChange={ev => setName(ev.target.value)}/>
           <input type="email" placeholder="your@email.com" value={email} onChange={ev => setEmail(ev.target.value)}/> 
           <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}/>
